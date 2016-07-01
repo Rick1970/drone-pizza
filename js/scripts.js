1feed71b2e383adfbed227f77f54c11bb1b3adab
Rick Thornbrugh <Rick Thornbrugh>
2016-07-01 16:57:55 -0700

    rework drop downs and buttons for data entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,38 @@
-// /business logic
 function Order(size, toppings){
-  this.size = size;
-  this.toppings = toppings;
-
+  this.size = "size";
+  this.toppings = [];
 }
+
+function Size(small, medium, large){
+  this.small = 8.00;
+  this.medium = 10.00;
+  this.large = 12.00;
+}
+
+function Toppings(mushroom, peperroni, hamburger){
+  this.mushroom = "mushroom";
+  this.peperroni = "peperroni";
+  this.hamburger = "hamburger";
+}
+
+
 
 
 $(document).ready(function(){
   $("form#place-order").submit(function(event){
     event.preventDefault();
-    var inputtedSize = $("input#new-size").val();
-    var inputtedTop = $("input#first-top").val();
-    var newOrder = new Order(inputtedSize, inputtedTop);
-    
+    var inputtedSize = $("#input1").val();
+    alert(inputtedSize);
+
+
+ $("button#top1").click(function(event){
+   var inputtedToppings = [];
+  inputtedToppings.push("peperroni");
+   event.preventDefault();
+  alert(inputtedToppings);
+  });
+
+
     $("ul#your-order").append("<li><span class='order'>" + newOrder.size + "</span></li>");
     $(".order").last().click(function(){
       $("#show-order").show();
