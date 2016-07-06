6c005db2ea3b45636b6538e055b76d3e7572c0b5
Rick Thornbrugh <Rick Thornbrugh>
2016-07-05 17:44:33 -0700

    order prints to the screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,27 @@
-function Order(size, toppings){
-  this.size = "size";
-  this.toppings = [];
+var sizeTotal;
+var toppingTotal;
+
+function Pizza (size, toppings){
+  this.size = size;
+  this.toppings = toppings;
 }
 
-function Size(small, medium, large){
-  this.small = 8.00;
-  this.medium = 10.00;
-  this.large = 12.00;
-}
-
-function Toppings(mushroom, peperroni, hamburger){
-  this.mushroom = "mushroom";
-  this.peperroni = "peperroni";
-  this.hamburger = "hamburger";
-}
+Pizza.prototype.currentOrder = function() {
+  return this.size + " " + this.toppings;
+};
 
 var inputtedToppings = [];
-
 
 $(document).ready(function(){
   $("form#place-order").submit(function(event){
     event.preventDefault();
     var inputtedSize = $("#input1").val();
-    alert(inputtedSize);
+    var inputtedTopping = $("#input2").val();
+    var newOrder = new Pizza(inputtedSize, inputtedTopping);
 
-  });
-
-  $("button#pep").click(function(event){
-    inputtedToppings.push("peperroni");
-    event.preventDefault();
-    alert(inputtedToppings);
-
-  });
-
-  $("button#mush").click(function(event){
-    inputtedToppings.push("mushroom");
-    event.preventDefault();
-    alert(inputtedToppings);
+    $("ul#final").append("<li>" + newOrder.currentOrder() + "</li>");
+    $("ul#finalprice").append("<li>" + "$" + newOrder.total() + "</li>");
 
 
   });
-
-  $("button#hamb").click(function(event){
-    inputtedToppings.push("hamburger");
-    event.preventDefault();
-    alert(inputtedToppings);
-
-  });
-    // $("ul#your-order").append("<li><span>" + newOrder.size + newOrder.toppings"</li>");
-
 });
-
-//     $(".order").last().click(function(){
-//       $("#show-order").show();
-//       $("#show-order h2").text(newOrder.size);
-//       $(".new-size").text(newOrder.size);
-//       $(".first-top").text(newOrder.toppings);
-//
-//     });
-//     $("input#new-size").val("");
-//     $("input#first-top").val("");
-//
-//   });
-// });
